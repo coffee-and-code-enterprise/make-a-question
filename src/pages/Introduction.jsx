72255967be32dfca214f54ca54b5e0ab8d6905f0
Vinicius Duarte <viniciusduartesant@gmail.com>
2025-09-03 13:58:20 -0300

    Settings e Intro criadas e adicionadas ao path

---
+++ src/pages/Introduction.jsx
@@ -0,0 +1,12 @@
+import styles from './Introduction.module.css';
+
+// Criando o componente da Introdução:
+function Introduction() {
+    return (
+        <main>
+            <h2>Introduction</h2>
+        </main>
+    )
+}   
+
+export default Introduction;

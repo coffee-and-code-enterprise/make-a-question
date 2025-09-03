e69a2838311f631ff51841a1c9f440bbc3032a28
Gustavo Henrique <gustavvohenrique58@gmail.com>
2025-09-03 13:57:07 -0300

    Report Bugs component has been acomplished by Gustavo Henrique de Almeida Silva

---
+++ src/pages/ReportBugs.jsx
@@ -0,0 +1,11 @@
+import styles from './ReportBugs.module.css'
+
+function ReportBugs() {
+    return (
+        <main>
+            <h2>Report Bugs</h2>
+        </main>
+    )
+}
+
+export default ReportBugs

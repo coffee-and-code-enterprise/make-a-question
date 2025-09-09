cdab4868556c70fb37f8cf184be7f355c73b204b
Guilherme Bitt. <guilherme.assis.bittencourt@gmail.com>
2025-09-09 15:51:19 -0300

    começando a tela de profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,95 @@
+// Dependencies
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+
+// Stylesheet
 import styles from "./Profile.module.css";
 
+// Criando o componente da Home:
 function Profile() {
-	return (
-		<main>
-			<h2>Profile</h2>
-		</main>
-	)
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <main className={styles.homeContainer}>
+      <section className={styles.userSection}>
+      </section>
+      <section className={styles.sectionSelection}>
+        <a
+          onClick={() => navigate("/home")}
+          className={location.pathname === "/home" && styles.selected}
+        >
+          Perguntas
+        </a>
+        <a
+          onClick={() => navigate("/home/foryou")}
+          className={location.pathname === "/home/foryou" && styles.selected}
+        >
+          Respostas
+        </a>
+        <a
+          onClick={() => navigate("/home/recentcomments")}
+          className={location.pathname === "/home/recentcomments" && styles.selected}
+        >
+          Favoritos
+        </a>
+      </section>
+      <section className={styles.questions}>
+        <div className={styles.buttons}>
+          <Link to={"#"}>
+            <button>Fazer uma pergunta</button>
+          </Link>
+          <Link to={"#"}>
+            <button>Responder uma pergunta aleatória</button>
+          </Link>
+        </div>
+				<div className={styles.comments}>
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<div className={styles.quComponents}>
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+								</div>
+							}
+						/>
+						<Route
+							path="foryou"
+							element={
+								<div className={styles.quComponents}>
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+								</div>
+							}
+						/>
+						<Route
+							path="recentcomments"
+							element={
+								<div className={styles.quComponents}>
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+									<div className={styles.exComponent} />
+								</div>
+							}
+						/>
+					</Routes>
+					<p>1 2 3 ... 218</p>
+				</div>
+      </section>
+    </main>
+  );
 }
 
-export default Profile
+// Exportando esse componente como padrão deste arquivo:
+export default Profile;

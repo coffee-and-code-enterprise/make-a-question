bdfd3ee24fe1d6de944b8bfa809dbfb418ae7732
Emanuel Resende <emanuelaugustosantos13@gmail.com>
2025-09-10 13:32:39 -0300

    Começando o ReportBugs

--- src/pages/ReportBugs.jsx
+++ src/pages/ReportBugs.jsx
@@ -2,8 +2,18 @@ import styles from './ReportBugs.module.css'
 
 function ReportBugs() {
     return (
-        <main>
-            <h2>Report Bugs</h2>
+        <main className={styles.hero}>
+            <section className={styles.heroContent}>
+                <h1 className={styles.heroTitle}>Relatar problema</h1>
+                <p className={styles.heroPa}>Encontrou um problema? Relate!</p>
+            </section>
+            <section className={styles.formSection}>
+                <h2>Nome</h2>
+                <h2>Email</h2>
+                <h2>Problema</h2>
+                <h2>Outro</h2>
+
+            </section>
         </main>
     )
 }

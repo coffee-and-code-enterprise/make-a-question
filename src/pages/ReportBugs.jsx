476e35805f24e4099242f39d7be7a7f42de4d0fd
Emanuel Resende <emanuelaugustosantos13@gmail.com>
2025-09-10 14:50:20 -0300

    Terminei o ReportBugs

--- src/pages/ReportBugs.jsx
+++ src/pages/ReportBugs.jsx
@@ -8,10 +8,39 @@ function ReportBugs() {
                 <p className={styles.heroPa}>Encontrou um problema? Relate!</p>
             </section>
             <section className={styles.formSection}>
-                <h2>Nome</h2>
-                <h2>Email</h2>
-                <h2>Problema</h2>
-                <h2>Outro</h2>
+                <form className={styles.formControl}>
+                    <div>
+                        <div><label htmlFor="Nome">Nome</label></div>
+                        <input type="text" placeholder='Nome Completo' required />
+                    </div>
+                    <div>
+                        <div>
+                            <br />
+                            <label htmlFor="Email">Email</label>
+                        </div>
+                        <input type="text" placeholder='Email' required />
+                    </div>
+                    <div>
+                        <br />
+                        <div><label htmlFor="Problema">Problema</label></div>
+                        <select name="Problema" id={styles.problem} required>
+                            <option value="">Selecionar problema</option>
+                            <option value="erro">Problemas em perguntar/responder</option>
+                            <option value="bug">Bugs Visuais</option>
+                            <option value="other">Outro</option>
+                        </select>
+                        <div>
+                            <br />
+                            <div><label htmlFor="Outro">Outro</label></div>
+                            <input className={styles.other} type="text" placeholder='Texto' required />
+                        </div>
+                    </div>
+                    <br />
+                    <button type='Submit' className={styles.btn}>Enviar
+                        
+                    </button>
+                </form>
+
 
             </section>
         </main>

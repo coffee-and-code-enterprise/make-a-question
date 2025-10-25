6197f5bf12e7423d2bd1e317e76d07f0a3ba411d
Guilherme Bitt. <guilherme.assis.bittencourt@gmail.com>
2025-10-24 22:32:04 -0300

    x

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://maq-api.ct.ws";
+const API_BASE_URL = "https://maq-api.ct.ws/public";
 
 // Criando uma instância do Axios
 const api = axios.create({
